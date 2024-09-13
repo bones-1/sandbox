cfe1e9a5782e0941afbdf1ea4dfa90bee05b5bb3
Douglas Campbell <telldouglas@gmail.com>
2024-09-13 02:47:33 -0500

    Current page: 167

--- php/vieworders.php
+++ php/vieworders.php
@@ -1,38 +1,71 @@
 <?php
 $company = "Bob's Auto Spare Parts";
 $title = "Order Results";
-?>
 
+
+$document_root = $_SERVER['DOCUMENT_ROOT'];
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $company . ' - ' . $title ?></title>
+    <title>Bob's Auto Parts - Customer Orders</title>
+    <style type="text/css">
+        table,
+        th,
+        td {
+            border-collapse: collapse;
+            border: 1px solid black;
+            padding: 6px;
+        }
+
+        th {
+            background: #ccccff;
+        }
+    </style>
+
 </head>
 
 <body>
-    <h1><?= $company ?></h1>
-    <h2><?= $title ?></h2>
-
+    <h1>Bob's Auto Parts</h1>
+    <h2>Customer Orders</h2>
     <?php
-//     @$fh = fopen("orders.txt", "rb");
-//     if (!$fh) {
-//         echo "<p><strong>No orders pending.<br />
-// Please try again later.</strong></p>";
-//         exit;
-//     }
-     $file_array = (file("orders.txt"));
-
-     foreach($file_array as $value){
-        echo $value . "</br>" ;
-     }
-
-    // fpassthru($fh);
-    // flock($fh, LOCK_UN);
-    // fclose($fh);
-
+    //Read in the entire file
+    //Each order becomes an element in the array
+    $orders = file("orders.txt");
+    // count the number of orders in the array
+    $number_of_orders = count($orders);
+    if ($number_of_orders == 0) {
+        echo "<p><strong>No orders pending.<br />
+Please try again later.</strong></p>";
+    }
+    echo "<table>\n";
+    echo "<tr>
+<th>Order Date</th>
+<th>Tires</th>
+<th>Oil</th>
+<th>Spark Plugs</th>
+<th>Total</th>
+<th>Address</th>
+<tr>";
+    for ($i = 0; $i < $number_of_orders; $i++) {
+        //split up each line
+        $line = explode("\t", $orders[$i]);
+        // keep only the number of items ordered
+        $line[1] = intval($line[1]);
+        $line[2] = intval($line[2]);
+        $line[3] = intval($line[3]);
+        // output each order
+        echo "<tr>
+<td>" . $line[0] . "</td>
+<td style=\"text-align: center;\">" . $line[1] . "</td>
+<td style=\"text-align: center;\">" . $line[2] . "</td>
+<td style=\"text-align: center;\">" . $line[3] . "</td>
+<td style=\"text-align: center;\">" . $line[4] . "</td>
+<td>" . $line[5] . "</td>
+</tr>";
+    }
+    echo "</table>";
     ?>
 </body>
 
