e9d27980a517c9d98fca4b71c57bde3679d0a03f
Douglas Campbell <telldouglas@gmail.com>
2024-09-25 18:26:41 -0500

    php practice
    practicing with classes

--- php/school.php
+++ php/school.php
@@ -1,121 +1,241 @@
     <?php
 
-    class Students
+    interface validate_person_interface
     {
-        protected $permitted_attributes = [
-            'name' => ['type' => 'string',],
-            'id' => ['type' => 'integer',],
-            'age' => ['type' => 'integer',],
-            'sex' => ['type' => 'string',],
-            'religion' => ['type' => 'string',],
-            'address' => ['type' => 'string',],
-            'debt' => ['type' => 'boolean',],
-        ];
+        # manipulate permitted_atributes array for selected class 
+        public function add_attribute(
+            string $name,
+            string $type,
+            array $properties
+        );
+        public function remove_attribute(string $name);
+        static function validate_data(array $data_array);
     }
 
-    class Classroom extends Students
+    # Validates self
+    // Creates objects that accepts self data array and validates it, returning try or false. can also add additional attributes for the data to be validated against
+    class Person implements validate_person_interface
     {
-        protected $student_db = [];
+        private const DATA_TYPES = ['boolean', 'integer', 'double',  'string', 'array'];
 
+        private const ALLOWED_PROPERTIES = [
+            'value',
+            'max',
+            'min',
+            'length',
+        ];
 
-        public function add_student($arr)
+        private const MAX_NAME_LEN = 20;
+
+        private const DEFAULT_RULES = [
+            'name' => [
+                'type' => 'string',
+                'length' => 50
+            ],
+            'id' => [
+                'type' => 'integer',
+                'min' => 100,
+                'max' => 999
+            ],
+            'age' => [
+                'type' => 'integer',
+                'min' => 15,
+                'max' => 150
+            ],
+            'sex' => [
+                'type' => 'string',
+                'value' => [
+                    'male',
+                    'female',
+                    'other'
+                ]
+            ],
+            'religion' => [
+                'type' => 'string',
+                'length' => 40
+            ],
+            'address' => [
+                'type' => 'string',
+                'lenght' => 150
+            ],
+        ];
+
+        protected $custom_rules;
+
+        public function add_attribute(string $name, string $type, array $properties = [])
         {
-            if ($this->validate_student_array($arr)) {
+            $name = trim(strtolower($name));
+            $type = trim(strtolower($type));
 
-                $not_already_added = $this->custom_arr_find($this->student_db, $arr['id'], "Classroom::compare_id") < 0;
+            $temp = [];
 
-                if ($not_already_added) {
-                    array_push($this->student_db, $arr);
-                    echo "Item added<br>";
-                }
-            }
-        }
-
-        public function show_student($id)
-        {
-            $student_index = $this->custom_arr_find($this->student_db, $id, "Classroom::compare_id");
-
-            if ($student_index < 0) {
-                echo "No such student.<br>";
-            } else {
-                echo "<br><em><strong>Student Info</strong></em><br>";
-                foreach ($this->student_db[$student_index] as $name => $value) {
-                    echo "$name: $value<br>";
-                }
-            }
-        }
-
-
-        protected static function compare_id($ar_value, $b)
-        {
-            return ($ar_value['id'] == $b);
-        }
-
-        final protected function validate_student_array($arr)
-        {
-            if (!(is_array($arr) && isset($arr['id']) && isset($arr['name']))) {
+            if (strlen($name) > self::MAX_NAME_LEN || !in_array($type, self::DATA_TYPES)) {
                 return false;
             }
 
-            if (!(is_int($arr['id']) && is_string($arr['name']))) {
-                return false;
-            }
+            if (count($properties) > 0) {
 
-            foreach (array_keys($arr) as $attr) {
-                if (!isset($this->permitted_attributes[$attr])) {
-                    return false;
-                }
+                foreach ($properties as $property_name => $property_value) {
 
-                if ($this->permitted_attributes[$attr]['type'] !== gettype($arr[$attr])) {
-                    return false;
+                    if (!in_array($property_name, self::ALLOWED_PROPERTIES)) {
+                        return false;
+                    }
+
+                    $temp[$property_name] = $property_value;
                 }
             }
 
+            $temp['type'] = $type;
+            $this->custom_rules[$name] = $temp;
+
+            echo var_dump($this->custom_rules);
             return true;
         }
 
-
-        // Traverses an array with callback function until it finds value and then returns the shallow index of that value. The callback function compares the two values returns true if they match and false if they do not
-        protected function custom_arr_find($arr, $value, $callback)
+        public function remove_attribute(string $name)
         {
-            foreach ($arr as $index => $arr_item) {
-
-                if ($callback($arr_item, $value) === true) {
-                    return $index;
-                };
-            }
-            return -1;
+            unset($this->custom_rules[$name]);
         }
 
-
-        # TODO: See what can be done about this 
-        function __get($name)
+        static function validate_data(array $data_array)
         {
-            if ($name !== 'total') {
-                print "No such property!<br>";
-                return;
+            if (!array_key_exists('id', $data_array)) {
+                return false;
             }
 
-            if (count($this->student_db) == 0) {
-                print "The classroom is empty.<br>";
-                return;
-            }
-
-            print "There is " . count($this->student_db) . ' students in this classroom.<br>';
-        }
-
-        function __set($name, $value)
-        {
-            print "Operation not permitted!<br>";
-
-            return false;
+            // Code to validate input using the conditions above
         }
     }
 
-    $room1 = new Classroom();
-    $room1->add_student(['id' => 132, 'name' => 'Sam', 'age' => 17, 'debt' => false]);
-    $room1->add_student(['id' => 3333, 'name' => 'Sam']);
-    $room1->show_student(132);
+    $student = new Person;
+
+    echo  $student->add_attribute('country', 'string', ['max' => 3]);
+    $student->remove_attribute('country');
+
+    echo 'done';
+    // interface student
+    // {
+    //     #inherits self
+    //      public function add_data(integer $id, array $data);
+    //      
+    // }
+
+    // interface teacher
+    // {
+    //     #inherits self
+    // }
+
+    // interface admin
+    // {
+    //     #inherits self
+    // }
+
+    // class Students implements student {}
+
+    // class Classroom extends Students
+    // {
+    //     protected $student_db = [];
 
 
+    //     public function add_student($arr)
+    //     {
+    //         if ($this->validate_student_array($arr)) {
+
+    //             $not_already_added = $this->custom_arr_find($this->student_db, $arr['id'], "Classroom::compare_id") < 0;
+
+    //             if ($not_already_added) {
+    //                 array_push($this->student_db, $arr);
+    //                 echo "Item added<br>";
+    //             }
+    //         }
+    //     }
+
+    //     public function show_student($id)
+    //     {
+    //         $student_index = $this->custom_arr_find($this->student_db, $id, "Classroom::compare_id");
+
+    //         if ($student_index < 0) {
+    //             echo "No such student.<br>";
+    //         } else {
+    //             echo "<br><em><strong>Student Info</strong></em><br>";
+    //             foreach ($this->student_db[$student_index] as $name => $value) {
+    //                 echo "$name: $value<br>";
+    //             }
+    //         }
+    //     }
+
+
+    //     protected static function compare_id($ar_value, $b)
+    //     {
+    //         return ($ar_value['id'] == $b);
+    //     }
+
+    //     final protected function validate_student_array($arr)
+    //     {
+    //         if (!(is_array($arr) && isset($arr['id']) && isset($arr['name']))) {
+    //             return false;
+    //         }
+
+    //         if (!(is_int($arr['id']) && is_string($arr['name']))) {
+    //             return false;
+    //         }
+
+    //         foreach (array_keys($arr) as $attr) {
+    //             if (!isset($this->RULES[$attr])) {
+    //                 return false;
+    //             }
+
+    //             if ($this->RULES[$attr]['type'] !== gettype($arr[$attr])) {
+    //                 return false;
+    //             }
+    //         }
+
+    //         return true;
+    //     }
+
+
+    //     // Traverses an array with callback function until it finds value and then returns the shallow index of that value. The callback function compares the two values returns true if they match and false if they do not
+    //     protected function custom_arr_find($arr, $value, $callback)
+    //     {
+    //         foreach ($arr as $index => $arr_item) {
+
+    //             if ($callback($arr_item, $value) === true) {
+    //                 return $index;
+    //             };
+    //         }
+    //         return -1;
+    //     }
+
+
+    //     # TODO: See what can be done about this 
+    //     function __get($name)
+    //     {
+    //         if ($name !== 'total') {
+    //             print "No such property!<br>";
+    //             return;
+    //         }
+
+    //         if (count($this->student_db) == 0) {
+    //             print "The classroom is empty.<br>";
+    //             return;
+    //         }
+
+    //         print "There is " . count($this->student_db) . ' students in this classroom.<br>';
+    //     }
+
+    //     function __set($name, $value)
+    //     {
+    //         print "Operation not permitted!<br>";
+
+    //         return false;
+    //     }
+    // }
+
+    // $room1 = new Classroom();
+    // $room1->add_student(['id' => 132, 'name' => 'Sam', 'age' => 17, 'debt' => false]);
+    // $room1->add_student(['id' => 3333, 'name' => 'Sam']);
+    // $room1->show_student(132);
+
+
+    // 
     ?>
